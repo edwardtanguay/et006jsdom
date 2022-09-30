3d95382f4dc084a648eb7a294c009040479e7206
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-30 10:15:44 +0200

    vscode sass working

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const contentElem = document.querySelector('.content');
 
 const books = [
 	{
-		title: "Psychology 101",
-		author: "George Anton"
+		title: 'Psychology 101',
+		author: 'George Anton'
 	},
 	{
-		title: "1984",
-		author: "George Orwell"
+		title: '1984',
+		author: 'George Orwell'
 	}
 ];
 
@@ -15,12 +15,16 @@ contentElem.innerHTML = `
 	<h1>Book Site</h1>
 	<h2>There are ${books.length} books:</h2>
 
-	<div className="books">
-		${books.map(book => {
-			return `
-			<div>${book.title}</div>
-			<div class="description">${book.author}</div>
+	<div class="books">
+		${books
+			.map((book) => {
+				return `
+			<div class="book">
+				<div class="title">${book.title}</div>
+				<div class="description">${book.author}</div>
+			</div>
 			`;
-		}).join('')}	
+			})
+			.join('')}	
 	</div>
-`; 
+`;
