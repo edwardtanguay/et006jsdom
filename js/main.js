2bf97c51a1474fb1bbc048c118147d5d982583e6
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-30 09:55:51 +0200

    two books are in

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const contentElem = document.querySelector('.content');
 
+const books = [
+	{
+		title: "Psychology 101",
+		author: "George Anton"
+	},
+	{
+		title: "1984",
+		author: "George Orwell"
+	}
+];
+
 contentElem.innerHTML = `
 	<h1>Book Site</h1>
-	<h2>There are nnn books:</h2>
+	<h2>There are ${books.length} books:</h2>
 `; 
