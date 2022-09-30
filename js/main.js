d30a6500583e376a406728223b7e241389e7aa2a
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-30 10:25:29 +0200

    created book module/model

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
+import { books } from './model.js';
 const contentElem = document.querySelector('.content');
-
-const books = [
-	{
-		title: 'Psychology 101',
-		author: 'George Anton'
-	},
-	{
-		title: '1984',
-		author: 'George Orwell'
-	}
-];
 
 contentElem.innerHTML = `
 	<h1>Book Site</h1>
---
+++ js/model.js
@@ -0,0 +1,10 @@
+export const books = [
+	{
+		title: 'Psychology 101',
+		author: 'George Anton'
+	},
+	{
+		title: '1984',
+		author: 'George Orwell'
+	}
+];
